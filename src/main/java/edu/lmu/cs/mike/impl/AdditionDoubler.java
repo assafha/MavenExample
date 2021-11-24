23dac1fa2966e9defdea21ebc3607cc265626f6a
assafha <92732128+assafha@users.noreply.github.com>
2021-11-24 18:25:35 +0200

    Update AdditionDoubler.java

--- src/main/java/edu/lmu/cs/mike/impl/AdditionDoubler.java
+++ src/main/java/edu/lmu/cs/mike/impl/AdditionDoubler.java
@@ -3,7 +3,8 @@ package edu.lmu.cs.mike.impl;
 import edu.lmu.cs.mike.iface.IntegerDoubler;
 
 public class AdditionDoubler implements IntegerDoubler {
-
+  
+  int a=0;
   @Override
   public long doDouble(int i) {
     return i + i;
